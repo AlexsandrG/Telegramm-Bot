a6ced095349de8c1062b12188ec86e9de6f2309b
AlexsandrG <faust335.ru@gmail.com>
2024-07-17 21:39:51 +0300

    initial commit

---
+++ run.py
@@ -0,0 +1,23 @@
+# 7302084975:AAEPi7jaA_Ipo80pjHAPuaqLb6xa9h1ZwLo
+from aiogram import Bot, Dispatcher, html
+from aiogram import filters
+import asyncio
+
+api = ('7302084975:AAEPi7jaA_Ipo80pjHAPuaqLb6xa9h1ZwLo')
+bot = Bot(token=api)
+
+
+class Memorystorage:
+    pass
+
+
+dp = Dispatcher(bot, storage= Memorystorage())
+
+@dp.message(commands=['start'])
+async def starter(message):
+    await message.answer(f"Привет, {html.bold(message.from_user.full_name)}!")
+
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
